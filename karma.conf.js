f603dc36b6fa67fcb856bb1231dff4bc03d21b16
ruuddeenen98@gmail.com <Rd&Mj!021216>
2020-05-23 14:23:16 +0200

    Added ChromeHeadlessCI customLauncher

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
